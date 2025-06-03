1625b9982bbd11e8b57afc36a48145fa7e71ebe2
ManarSwar97 <manarswar97@gmail.com>
2025-06-03 16:29:25 +0300

    sign in done

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,5 +1,4 @@
 import Client from './api'
-
 export const RegisterUser = async (data) => {
   try {
     const res = await Client.post('/auth/register', data)
@@ -8,5 +7,22 @@ export const RegisterUser = async (data) => {
     throw error
   }
 }
-
-
+export const SignInUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/login', data)
+    // Set the current signed in users token to localStorage
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+// export const CheckSession = async () => {
+//   try {
+//     // Checks if the current token if it exists is valid
+//     const res = await Client.get('/auth/session')
+//     return res.data
+//   } catch (error) {
+//     throw error
+//   }
+// }
