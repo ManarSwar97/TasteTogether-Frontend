80f9dfdb15e5edeabc6338b18deab6933f642cd1
ManarSwar97 <manarswar97@gmail.com>
2025-06-04 10:56:11 +0300

    modify create post functionality

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,17 @@
-const Post = ({posts}) =>{
-    return (
-    <div className="post-list">
-        {posts.map((post, index) => (
-          <div key={index} className="post-card">
-            <img src={`http://localhost:3001/uploads/${post.postImage}`} alt="Post" />
-            <p>{post.postDescription}</p>
-          </div>
-        ))}
-      </div>
-    )
+import { Link } from "react-router-dom"
+const Post = ({ post }) => {
+  return (
+    <div className="post-card">
+      <img src={`http://localhost:3001/uploads/${post.postImage}`} alt="Post" />
+      <p style={{ color: 'black' }}>{post.postDescription}</p>
+      <Link to="/update">
+      <button>Edit Post</button>
+      </Link>
+      <Link to="/delete">
+      <button>Delete Post</button>
+      </Link>
+    </div>
+  )
 }
+
 export default Post
---
+++ src/pages/UpdatePost.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import axios from 'axios'
+import { useNavigate } from "react-router-dom"
+const UpdatePost = ({ addPost }) =>{
+    let navigate = useNavigate()
+    const initialState = {
+        postImage: '',
+        postDescription: '',
+    }
+    const [postState, setPostState] = useState(initialState)
+        const handleChange = (event) => {
+        const { id, value, files } = event.target
+        setPostState({
+            ...postState,
+            [id]: files ? files[0] : value
+        })
+        }
+
+const handleSubmit = async (event) => {
+  event.preventDefault()
+  const formData = new FormData()
+  formData.append('postImage', postState.postImage)
+  formData.append('postDescription', postState.postDescription)
+
+  const token = localStorage.getItem('token')
+
+  const response = await axios.post('http://localhost:3001/posts', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      Authorization: `Bearer ${token}`
+    }
+  })
+  const newPost = response.data
+  addPost(newPost)
+  setPostState(initialState)
+  navigate('/main')
+
+  }
+    return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="postImage">Post Image:</label>
+      <input
+        id="postImage"
+        type="file"
+        onChange={handleChange}
+      />
+      <label htmlFor="postDescription">Post Description:</label>
+      <input
+        id="postDescription"
+        type="text"
+        onChange={handleChange}
+        value={postState.postDescription}
+      />
+      <button type="submit">Create Post</button>
+    </form>
+  )
+}
+
+export default UpdatePost
